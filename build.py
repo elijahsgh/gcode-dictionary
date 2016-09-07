c1fd585c27c01a55ce10ae98fdfa7bb2f9b85d87
Elijah Snyder <elijah.snyder1@gmail.com>
2016-09-06 20:12:39 -0500

    Updating build script and built dictionary

--- build.py
+++ build.py
@@ -3,16 +3,16 @@
 import json
 import os
 
-gcode_dictionary = {}
+gcode_dictionary = []
 
 for deffile in os.listdir('files/'):
-  if deffile.endswith(".json"):
+  if deffile.endswith(".json") and not deffile.startswith('template'):
     with open('files/' + deffile) as f:
       gcode_definition = json.load(f)
-      gcode_dictionary[gcode_definition['code']] = gcode_definition
+      gcode_dictionary.append(gcode_definition)
 
 with open('gcode_dictionary.json', 'w') as f:
   f.write(json.dumps(gcode_dictionary,
                      sort_keys=True,
-                     indent=2, 
+                     indent=2,
                      separators=(',', ': ')))
