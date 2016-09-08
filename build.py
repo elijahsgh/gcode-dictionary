4fe151baed6a2c5521e00cb3c2f34aa7c4cfa4b9
Elijah Snyder <elijah.snyder1@gmail.com>
2016-09-07 22:18:58 -0500

    Updating schema again

--- build.py
+++ build.py
@@ -3,13 +3,13 @@
 import json
 import os
 
-gcode_dictionary = []
+gcode_dictionary = {}
 
 for deffile in os.listdir('files/'):
   if deffile.endswith(".json") and not deffile.startswith('template'):
     with open('files/' + deffile) as f:
       gcode_definition = json.load(f)
-      gcode_dictionary.append(gcode_definition)
+      gcode_dictionary.update(gcode_definition)
 
 with open('gcode_dictionary.json', 'w') as f:
   f.write(json.dumps(gcode_dictionary,
